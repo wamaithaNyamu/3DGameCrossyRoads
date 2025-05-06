b23755a809f536c179f13070cb8c302eaa183142
Wamaitha <wamaithanyamu@gmail.com>
2025-05-06 13:07:18 +0300

    feat: made  a scene and a box character. Setup lights and camera

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 3DGameCrossyRoads
+
+This game follows the tutorial found [here](https://javascriptgametutorials.com/)
---
+++ crossyroads/src/Game.tsx
@@ -0,0 +1,10 @@
+import { Scene } from "./components/Scene";
+import { Player } from "./components/Player";
+
+export default function Game() {
+  return (
+    <Scene>
+      <Player />
+    </Scene>
+  );
+}
